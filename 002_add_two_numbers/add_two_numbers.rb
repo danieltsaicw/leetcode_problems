36fed9a3dd6264f9fa7d6c8256440a2f939110a0
Daniel Tsai C.W <daniel.tsai.cw@gmail.com>
2019-01-20 07:55:11 -0800

    add_two_numbers_rb

--- 002_add_two_numbers/add_two_numbers.rb
+++ 002_add_two_numbers/add_two_numbers.rb
@@ -1,6 +1,49 @@
-class Node
-	attr_accessor :value
-	attr_accessor :next
-end
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
 
-node = Node.new
+# @param {ListNode} l1
+# @param {ListNode} l2
+# @return {ListNode}
+
+    
+
+def add_two_numbers(l1, l2)
+
+
+    carry, remainder = (l1.val + l2.val).divmod(10)
+    sum = ListNode.new(remainder)
+    sum_from_node_1 = sum
+    l1 = l1.next
+    l2 = l2.next
+
+
+    while (l1 || l2 || carry > 0)
+        single_sum = carry
+        if l1
+            single_sum += l1.val
+            l1 = l1.next
+        end
+        if l2
+            single_sum += l2.val
+            l2 = l2.next
+        end
+
+        carry, remainder = single_sum.divmod(10)
+
+        sum.next = ListNode.new(remainder)
+        sum = sum.next
+
+
+    end
+
+
+    return sum_from_node_1
+
+
+end
